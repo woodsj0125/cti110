ad7a7a543e6af322f49835ccf1338d2107fc7ca5
woodsj0125 <44269483+woodsj0125@users.noreply.github.com>
2018-11-08 12:26:38 -0500

    Add files via upload

---
+++ P2Practice_Woods.py
@@ -0,0 +1,20 @@
+
+
+ # For Practice
+ #Ask user their age
+ #Tell them how old they will be next year.
+ #CTI 110 #Assigment
+ #CTI 110 #Assigment
+ # Javonte Woods
+#11/8/2018
+# Header
+ # Get input number=int(input("Enter number:"))
+ #Calculate the answear answear = number*2
+ #Print output print("Your result is",answear)
+
+print("What is your age")
+age = int(input("Enter age: "))
+
+yourAgeIs = ( age + 1)            
+
+print("Your age is" , yourAgeIs)
