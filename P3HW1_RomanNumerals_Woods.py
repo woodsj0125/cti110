66deb5ec129f014fa5933b05f406771f225c0df8
woodsj0125 <44269483+woodsj0125@users.noreply.github.com>
2018-12-13 11:54:16 -0500

    Add files via upload

---
+++ P3HW1_RomanNumerals_Woods.py
@@ -0,0 +1,53 @@
+#CTI-110
+#P3HW1 - Roman Numerals
+# Javonte Woods
+#11-26-2018
+#programs should should show Roman Numeral of the given number given from 1-10
+A_number = 1
+B_number = 2
+C_number = 3
+D_number = 4
+E_number = 5
+F_number = 6
+G_number = 7
+H_number = 8
+I_number = 9
+J_number = 10
+number = int(input("Enter number"))
+if number == A_number:
+    print("I")
+    
+if number == B_number:
+    print("II")
+            
+if number == C_number:
+    print("III")
+                    
+if number == D_number:
+    print("IV")
+
+if number == E_number:
+    print("V")
+                                    
+if number == F_number:
+    print("VI")
+                                            
+if number == G_number:
+    print("VII")
+                                                    
+if number == H_number:
+    print("VIII")
+                                                            
+if number == F_number:
+    print("IX")
+                                                                    
+if number == J_number:
+    print("X")
+    
+if number > J_number:
+     
+    print("Invalid Number")
+  
+    
+								
+
