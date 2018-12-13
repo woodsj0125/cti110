962a64ea2c08e5ddd93f62bea982ec5ad75134a9
woodsj0125 <44269483+woodsj0125@users.noreply.github.com>
2018-12-13 12:27:00 -0500

    Add files via upload

---
+++ P3T1-Areas Of Rectangles.py
@@ -0,0 +1,14 @@
+Python 3.4.0 (v3.4.0:04f714765c13, Mar 16 2014, 19:25:23) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+What is the lenghth of side A and B
+lengthA :25
+LengthB :45
+1125
+what is the width of Side A and B
+WidthA:55
+WidthB:125
+5625
+AreaB is larger
+>>> 
