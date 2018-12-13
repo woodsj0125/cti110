d351ab70002f6cd1cdfdf938d0e0d7e5895c5ae1
woodsj0125 <44269483+woodsj0125@users.noreply.github.com>
2018-12-13 11:53:57 -0500

    Add files via upload

---
+++ P2T1_SalesPrediction_FirstLast.py
@@ -0,0 +1,21 @@
+#CTI 110
+#P2T1
+#Javonte Woods
+#11-1-2018
+#Sales Prediction Code
+
+studentName='Bob'
+studentGPA=3.2
+studentAGE=25
+print ("Hello",studentName)
+
+print ("Hello",studentName)
+
+total_sales =input ('Enter the projected sales: ')
+total_sales = float(input('Enter the projected sales: '))
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+#Display the profit.
+print('The profit is $', profit)
+
